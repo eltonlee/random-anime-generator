7cb2951ebe156e36fe8c1858dbbfa8d6d7e6fb10
Elton Lee <eltonlee.w@gmail.com>
2021-07-15 04:21:50 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,37 @@ import CenterLogo from './components/CenterLogo';
 import './index.css'
 import React from 'react';
 import Button from './components/button';
+import { useState, useEffect} from 'react';
+//import AnimeCard from './components/AnimeCard';
+
 
 
 function App() {
+
+  const [anime, SetAnime] = useState([]);
+
+  const getAnime = async () => {
+    const temp = await fetch('https://api.jikan.moe/v3/anime/1')
+      .then(res => res.json());
+    
+    SetAnime(temp);
+  }
+
+  useEffect(() => {
+    getAnime();
+    
+    
+  }, [])
+
+  console.log(anime)
+
   return (
     
     <React.Fragment>
     <CenterLogo />
     <Button />
+    < img src={anime.image_url} alt="" />
+
     </React.Fragment>
     
     
