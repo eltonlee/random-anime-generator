6db8b68c72ac7e36898897de0e7676fb290771df
Elton Lee <eltonlee.w@gmail.com>
2021-07-14 20:26:57 -0700

    Initial Commit

---
+++ src/CenterLogo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CenterLogo() {
+    return (
+        <div className='centerlogo'>
+            <h1>Random Anime Generator </h1> 
+        </div>
+    )
+}
---
+++ src/button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button() {
+    return (
+        <div className='btn'>
+            <button className='btn'>Randomize!</button>
+        </div>
+    )
+}
